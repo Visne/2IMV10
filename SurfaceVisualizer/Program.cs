436f1cf8d77594454b0b8982b01a44b0bb7ab3d1
Visne <39844191+Visne@users.noreply.github.com>
2024-02-26 18:10:50 +0100

    Windowing

--- SurfaceVisualizer/Program.cs
+++ SurfaceVisualizer/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello world!");
+﻿using SurfaceVisualizer;
+
+using var window = new Window("Surface Visualizer", 1200, 800);
+
+window.Run();
---
+++ SurfaceVisualizer/Window.cs
@@ -0,0 +1,49 @@
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace SurfaceVisualizer;
+
+public class Window(string title, int width, int height) : GameWindow(GameWindowSettings.Default,
+    new NativeWindowSettings
+    {
+        APIVersion = new Version(4, 5),
+        Title = title,
+        ClientSize = new Vector2i(width, height),
+    })
+{
+    protected override void OnLoad()
+    {
+        base.OnLoad();
+
+        GL.ClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+    }
+    
+    protected override void OnRenderFrame(FrameEventArgs e)
+    {
+        base.OnRenderFrame(e);
+
+        GL.Clear(ClearBufferMask.ColorBufferBit);
+
+        SwapBuffers();
+    }
+
+    protected override void OnUpdateFrame(FrameEventArgs e)
+    {
+        base.OnUpdateFrame(e);
+        
+        if (KeyboardState.IsKeyReleased(Keys.Escape))
+        {
+            Close();
+        }
+    }
+
+    protected override void OnResize(ResizeEventArgs e)
+    {
+        base.OnResize(e);
+
+        GL.Viewport(0, 0, e.Width, e.Height);
+    }
+}
