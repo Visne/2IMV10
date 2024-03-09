75654cd528f58f9be28fcd489c76bc5ecca3f616
JorisParee <joris.paree@gmail.com>
2024-03-09 22:20:43 +0100

    cleaning up moddelSplitter

--- ModelSplitter/Program.cs
+++ ModelSplitter/Program.cs
@@ -5,25 +5,25 @@ var model = ModelRoot.Load("/Users/joris/Documents/Models/cube.glb");
 var mesh = model.DefaultScene.VisualChildren.Single().Mesh;
 var primitive = mesh.Primitives.Single();
 
-Console.WriteLine("Begin of ModelSplitter project");
+float[] splitHeights = [float.MinValue, 0f, float.MaxValue];
 
-float[] splitHeights = [float.MinValue, -1f, 0f, 1f, float.MaxValue];
+ModelSplitter.SplitModel(primitive.GetTriangles(), splitHeights, true);
 
-List<List<(Vector3, Vector3, Vector3, String)>> models = new List<List<(Vector3, Vector3, Vector3, String)>>();
-
-for (int i = 1; i < splitHeights.Length; i++ )
+class ModelSplitter
 {
-    var low = splitHeights[i - 1];
-    var high = splitHeights[i];
-    List<(Vector3, Vector3, Vector3, String)> splitTriangles = new List<(Vector3, Vector3, Vector3, String)>();
 
-    Console.WriteLine("now at height" + i);
-
-
-
-    foreach (var (a1, b1, c1) in primitive.GetTriangles())
+static public List<List<(Vector3, Vector3, Vector3)>> SplitModel (IEnumerable<(Vector3, Vector3, Vector3)> model, float[] planes, bool print = false)
     {
-        Console.WriteLine("in the tiangles");
+        List<List<(Vector3, Vector3, Vector3)>> models = new List<List<(Vector3, Vector3, Vector3)>>();
+
+        for (int i = 1; i < planes.Length; i++ )
+{
+    var low = planes[i - 1];
+    var high = planes[i];
+    List<(Vector3, Vector3, Vector3)> splitTriangles = new List<(Vector3, Vector3, Vector3)>();
+
+    foreach (var (a1, b1, c1) in model)
+    {
         var a = a1;
         var b = b1;
         var c = c1;
@@ -46,96 +46,94 @@ for (int i = 1; i < splitHeights.Length; i++ )
             a = z;
         }
         //Sorted the values, now a is smallest and c is highest
-        Console.WriteLine("aftersort");
 
         if (c.Y <= low || a.Y >= high)
         {
             //Outside of current split
-            Console.WriteLine("outside split");
         }
         else
         if (a.Y >= low & c.Y <= high)
         {
             //Fully inside current split
-            Console.WriteLine("within split");
-            splitTriangles.Add((a, b, c, "within"));
+            splitTriangles.Add((a, b, c));
         }
         //Cases where the triangles intersect the line
         else
         if (a.Y < low)
         {
-            Console.WriteLine("intersect bottom");
             if (b.Y > low)
             {
                 //One vertex below bottom
-                Console.WriteLine("pointing down");
-                splitTriangles.Add((getIntersection(a, c, low), b, c, "bottomDown1"));
-                splitTriangles.Add((getIntersection(a, c, low), getIntersection(a, b, low), b, "BottomDown2"));
+                splitTriangles.Add((helperFunctions.getIntersection(a, c, low), b, c));
+                splitTriangles.Add((helperFunctions.getIntersection(a, c, low), helperFunctions.getIntersection(a, b, low), b));
             }
             else
             {
                 //Two vertices below bottom
-                Console.WriteLine("pointing up");
-                splitTriangles.Add((getIntersection(a, c, low), getIntersection(b, c, low), c, "BottomUp"));
+                splitTriangles.Add((helperFunctions.getIntersection(a, c, low), helperFunctions.getIntersection(b, c, low), c));
             }
         }
         else
         if (c.Y > high)
         {
-            Console.WriteLine("Intersect top");
             if(b.Y < high)
             {
                 //One vertex Above top
-                Console.WriteLine("pointing up");
-                splitTriangles.Add((getIntersection(a, c, high), a, b, "topUp1"));
-                splitTriangles.Add((getIntersection(a, c, high), getIntersection(b, c, high), b, "TopUp2"));
+                splitTriangles.Add((helperFunctions.getIntersection(a, c, high), a, b));
+                splitTriangles.Add((helperFunctions.getIntersection(a, c, high), helperFunctions.getIntersection(b, c, high), b));
             }
             else
             {
                 //Two vertices Above top
-                Console.WriteLine("pointing down");
-                Console.WriteLine("values , "+ getIntersection(a, c, high).ToString()+ " , " + a + " , " + b + " , " + c + " , " + a1 + " , " + b1 +" , "+c1); 
-                splitTriangles.Add((getIntersection(a, c, high), getIntersection(a, b, high), a, "TopDown"));
+                splitTriangles.Add((helperFunctions.getIntersection(a, c, high), helperFunctions.getIntersection(a, b, high), a));
             }
         }
-        Console.WriteLine("what edgecase did i forget?");
     }
-    Console.WriteLine("afterTriangles");
     models.Add(splitTriangles);
 }
-Console.WriteLine("total finish");
 
-printModels(models);
-
-
-static void printModels(List<List<(Vector3 A, Vector3 B, Vector3 C, String)>> themodel)
-{
-    var modelArray = themodel.ToArray();
-    Console.WriteLine(modelArray.Length);
-    int i = 0;
-    foreach (var model in themodel)
-    {
-        i++;
-        int j = 0;
-        foreach (var triangle in model)
+        if (print)
         {
-            j++;
-            Console.WriteLine("model: " + i + " triangle: " + j+ " values: " +triangle.ToString());
+            helperFunctions.printModels(models);
+        }
+        return models;
+
+    }
+}
+
+class helperFunctions
+{
+    static public Vector3 getIntersection(Vector3 A, Vector3 B, float Height)
+    {
+        float totalLength = Vector3.Distance(A, B);
+        float t = (Height - A.Y) / (B.Y - A.Y);
+
+        Vector3 result = Vector3.Lerp(A, B, t);
+
+        return result;
+    }
+    static public void printModels(List<List<(Vector3 A, Vector3 B, Vector3 C)>> themodel)
+    {
+        var modelArray = themodel.ToArray();
+        Console.WriteLine(modelArray.Length);
+        int i = 0;
+        foreach (var model in themodel)
+        {
+            int j = 0;
+            foreach (var triangle in model)
+            {
+                Console.WriteLine("model: " + i + " triangle: " + j + " values: " + triangle.ToString());
+                j++;
+            }
+            i++;
         }
     }
 }
 
 
 
-static Vector3 getIntersection(Vector3 A, Vector3 B, float Height)
-{
-    float totalLength = Vector3.Distance(A, B);
-    float t = (Height - A.Y) / (B.Y - A.Y);
 
-    Vector3 result = Vector3.Lerp(A, B, t);
 
-    return result;
-}
 
 
 
